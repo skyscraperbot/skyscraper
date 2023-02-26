8ce1ef6b7c786f7d981f42b61a8214f1e3e3fc8e
Bren Straub <brstraub@wccnet.edu>
2023-02-25 19:38:09 -0500

    Created PingPong example class

--- src/main/java/org/skyscraper/PingPong.java
+++ src/main/java/org/skyscraper/PingPong.java
@@ -1,20 +1,22 @@
-package main.java.org.skyscraper; //Specify the package, which will almost always be this
+package org.skyscraper;
 
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel; //Remember to import libraries that you're using
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class PingPong extends ListenerAdapter {
     @Override
-    public void onMessageReceived(MessageRecievedEvent event) {
-        TextChannel channel = event.getTextChannel();
+    public void onMessageReceived(MessageReceivedEvent event) {
+        //TextChannel channel = event.getTextChannel();
 
         String text = event.getMessage().getContentDisplay();
         text = text.toLowerCase();
 
+        System.out.println("text");
+
         if (text == "ping") {
-            channel.sendMessage("Pong");
+            //channel.sendMessage("Pong");
+            event.getTextChannel().sendMessage("Pong");
         }
     }
 }

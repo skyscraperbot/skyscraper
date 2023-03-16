37ba98433dfd0a0b893008f6e2c876ee59c6b5a3
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-03-16 18:54:59 -0400

    Testing

--- src/main/java/org/skyscraper/SPCCommand.java
+++ src/main/java/org/skyscraper/SPCCommand.java
@@ -6,19 +6,23 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.TextChannel;
 
 public class SPCCommand extends ListenerAdapter {
 	private void showOutlook(SlashCommandEvent event,boolean isPrivate) {
 		
+		System.out.println("Making embed");
+		
 		MessageEmbed newEmbed = new EmbedBuilder()
-				.setImage("https://www.spc.noaa.gov/products/activity_loop.gif") //Simple reference to their resource file
+				//Simple reference to their resource file
+				//.setImage("https://www.spc.noaa.gov/products/activity_loop.gif?" + Long.toString(Math.round(Math.random() * 100000))) //Append meaningless query to escape previously cached image
+				.setImage("https://www.spc.noaa.gov/products/activity_loop.gif")
 				.build();
 		
 		event.replyEmbeds(newEmbed).queue();
 	}
 	
 	public CommandData getCommandData() {
+		
 		return new CommandData("spc","Get data from the NOAA Storm Prediction Center")
 			.addOption(OptionType.STRING,"type","What type of info to fetch, e.g. \'help\'")
 			.addOption(OptionType.BOOLEAN,"private","Whether or not the message should be private");
@@ -26,9 +30,14 @@ public class SPCCommand extends ListenerAdapter {
 	
 	@Override
 	public void onSlashCommand(SlashCommandEvent event) {
+		System.out.println("Registered change 1");
+		
 		if (event.getName().equals("spc")) {
 			String type = event.getOption("type").getAsString();
 			boolean isPrivate = event.getOption("private").getAsBoolean();
+			
+			System.out.println(type);
+			System.out.println(isPrivate);
 			
 			switch (type) {
 			case "help":

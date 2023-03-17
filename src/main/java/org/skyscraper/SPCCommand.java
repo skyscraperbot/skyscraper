fbb6a412c4e41a08253141559f572cf894bb5521
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-03-16 21:18:45 -0400

    Moved help into a function, created empty buttons for outlook

--- src/main/java/org/skyscraper/SPCCommand.java
+++ src/main/java/org/skyscraper/SPCCommand.java
@@ -4,21 +4,37 @@ import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.components.Button;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
 public class SPCCommand extends ListenerAdapter {
+	private void showHelp(SlashCommandEvent event,boolean isPrivate) {
+		event.reply("Arguments:\n"
+				+ "**type** - Return the type of requested information\n"
+				+ "```\n"
+				+ "help: Return a list of appropriate arguments\n"
+				+ "outlook: Return the convective weather outlook\n"
+				+ "```"
+				+ "**private** - Make the destination message private\n"
+				).queue();
+	}
+	
 	private void showOutlook(SlashCommandEvent event,boolean isPrivate) {
-		
-		System.out.println("Making embed");
-		
 		MessageEmbed newEmbed = new EmbedBuilder()
 				//Simple reference to their resource file
 				.setImage("https://www.spc.noaa.gov/products/activity_loop.gif?" + Long.toString(Math.round(Math.random() * 100000))) //Append meaningless query to escape previously cached image
-				//.setImage("https://www.spc.noaa.gov/products/activity_loop.gif")
 				.build();
 		
-		event.replyEmbeds(newEmbed).queue();
+		event.replyEmbeds(newEmbed)
+			.addActionRow(
+					Button.secondary("day0otlk","Day 0"),
+					Button.secondary("day1otlk", "Day 1"),
+					Button.secondary("day2otlk", "Day 2"),
+					Button.secondary("day3otlk", "Day 3"),
+					Button.secondary("day48otlk", "Day 4-8")
+					)
+		.queue();
 	}
 	
 	public CommandData getCommandData() {
@@ -41,16 +57,11 @@ public class SPCCommand extends ListenerAdapter {
 			
 			switch (type) {
 			case "help":
-				event.reply("Arguments:\n"
-						+ "**type** - Return the type of requested information\n"
-						+ "```\n"
-						+ "help: Return a list of appropriate arguments\n"
-						+ "outlook: Return the convective weather outlook\n"
-						+ "```"
-						+ "**private** - Make the destination message private\n"
-						);
+				showHelp(event, isPrivate);
+				break;
 			case "outlook":
 				showOutlook(event, isPrivate);
+				break;
 			default:
 				event.reply("You must specify an information type! Refer to /help for information.").setEphemeral(true);
 			}

dfd66c250b0fab7a562e77f32db3368ac89956f0
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-03-16 22:16:39 -0400

    Added support for 4-8 day convective outlook

--- src/main/java/org/skyscraper/SPCCommand.java
+++ src/main/java/org/skyscraper/SPCCommand.java
@@ -1,5 +1,6 @@
 package org.skyscraper;
 
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -10,6 +11,8 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 
 public class SPCCommand extends ListenerAdapter {
 	private void showHelp(SlashCommandEvent event,boolean isPrivate) {
+		System.out.println("Recognized help func");
+		
 		event.reply("Arguments:\n"
 				+ "**type** - Return the type of requested information\n"
 				+ "```\n"
@@ -21,6 +24,8 @@ public class SPCCommand extends ListenerAdapter {
 	}
 	
 	private void showOutlook(SlashCommandEvent event,boolean isPrivate) {
+		System.out.println("Recognized outlook func");
+		
 		MessageEmbed newEmbed = new EmbedBuilder()
 				//Simple reference to their resource file
 				.setImage("https://www.spc.noaa.gov/products/activity_loop.gif?" + Long.toString(Math.round(Math.random() * 100000))) //Append meaningless query to escape previously cached image
@@ -28,7 +33,7 @@ public class SPCCommand extends ListenerAdapter {
 		
 		event.replyEmbeds(newEmbed)
 			.addActionRow(
-					Button.secondary("day0otlk","Day 0"),
+					Button.primary("day0otlk","Day 0"),
 					Button.secondary("day1otlk", "Day 1"),
 					Button.secondary("day2otlk", "Day 2"),
 					Button.secondary("day3otlk", "Day 3"),
@@ -46,9 +51,11 @@ public class SPCCommand extends ListenerAdapter {
 	
 	@Override
 	public void onSlashCommand(SlashCommandEvent event) {
-		System.out.println("Registered change 1");
+		System.out.println("Registered change 2");
 		
 		if (event.getName().equals("spc")) {
+			System.out.println("spc recog");
+			
 			String type = event.getOption("type").getAsString();
 			boolean isPrivate = event.getOption("private").getAsBoolean();
 			
@@ -57,9 +64,11 @@ public class SPCCommand extends ListenerAdapter {
 			
 			switch (type) {
 			case "help":
+				System.out.println("Registering help");
 				showHelp(event, isPrivate);
 				break;
 			case "outlook":
+				System.out.println("Registering outlook");
 				showOutlook(event, isPrivate);
 				break;
 			default:
@@ -67,6 +76,25 @@ public class SPCCommand extends ListenerAdapter {
 			}
 		} else {
 			return;
+		}
+	}
+	
+	@Override
+	public void onButtonClick(ButtonClickEvent event) {
+		if (event.getComponentId().equals("day48otlk")) {
+			MessageEmbed newEmbed = new EmbedBuilder()
+					//Simple reference to their resource file
+					.setImage("https://www.spc.noaa.gov/products/outlook/day3otlk_0730.gif?" + Long.toString(Math.round(Math.random() * 100000))) //Append meaningless query to escape previously cached image
+					.build();
+			
+			event.editMessageEmbeds(newEmbed).queue();
+			event.editComponents().setActionRow(
+					Button.secondary("day0otlk","Day 0"),
+					Button.secondary("day1otlk", "Day 1"),
+					Button.secondary("day2otlk", "Day 2"),
+					Button.secondary("day3otlk", "Day 3"),
+					Button.primary("day48otlk", "Day 4-8")
+					);
 		}
 	}
 }

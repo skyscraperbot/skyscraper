e919d6d84caaa25903126e4598dad5b00cacd5a0
brstraub@wccnet.edu <89931838+BrenCS-WCC@users.noreply.github.com>
2023-03-22 18:04:13 -0400

    Testing button changes

--- src/main/java/org/skyscraper/SPCCommand.java
+++ src/main/java/org/skyscraper/SPCCommand.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.components.Button;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
 
 public class SPCCommand extends ListenerAdapter {
 	private void showHelp(SlashCommandEvent event,boolean isPrivate) {
@@ -20,11 +21,12 @@ public class SPCCommand extends ListenerAdapter {
 				+ "outlook: Return the convective weather outlook\n"
 				+ "```"
 				+ "**private** - Make the destination message private\n"
-				).queue();
+				).setEphemeral(isPrivate).queue();
 	}
 	
 	private void showOutlook(SlashCommandEvent event,boolean isPrivate) {
 		System.out.println("Recognized outlook func");
+		//User user = event.getUser();
 		
 		MessageEmbed newEmbed = new EmbedBuilder()
 				//Simple reference to their resource file
@@ -39,7 +41,7 @@ public class SPCCommand extends ListenerAdapter {
 					Button.secondary("day3otlk", "Day 3"),
 					Button.secondary("day48otlk", "Day 4-8")
 					)
-		.queue();
+		.setEphemeral(isPrivate).queue();
 	}
 	
 	public CommandData getCommandData() {
@@ -51,28 +53,27 @@ public class SPCCommand extends ListenerAdapter {
 	
 	@Override
 	public void onSlashCommand(SlashCommandEvent event) {
-		System.out.println("Registered change 2");
+		System.out.println("Registered change 4");
 		
 		if (event.getName().equals("spc")) {
-			System.out.println("spc recog");
-			
-			String type = event.getOption("type").getAsString();
-			boolean isPrivate = event.getOption("private").getAsBoolean();
-			
-			System.out.println(type);
-			System.out.println(isPrivate);
-			
-			switch (type) {
-			case "help":
-				System.out.println("Registering help");
-				showHelp(event, isPrivate);
-				break;
-			case "outlook":
-				System.out.println("Registering outlook");
-				showOutlook(event, isPrivate);
-				break;
-			default:
-				event.reply("You must specify an information type! Refer to /help for information.").setEphemeral(true);
+			System.out.println("Identified Event");
+			try {
+				System.out.println("Attempting Code");
+				String type = event.getOption("type").getAsString();
+				boolean isPrivate = event.getOption("private").getAsBoolean();
+				
+				switch (type) {
+				case "help":
+					showHelp(event, isPrivate);
+					break;
+				case "outlook":
+					showOutlook(event, isPrivate);
+					break;
+				default:
+					event.reply("You must specify an information type! Refer to /help for information.").setEphemeral(true).queue();
+				}
+			} catch (NullPointerException err) {
+				event.reply("Missing arguments: Please refer to /help for more information.").setEphemeral(true).queue();
 			}
 		} else {
 			return;
@@ -81,20 +82,27 @@ public class SPCCommand extends ListenerAdapter {
 	
 	@Override
 	public void onButtonClick(ButtonClickEvent event) {
+		event.editComponents().setActionRow(
+					(event.getComponentId().equals("day0otlk") ? Button.primary("day0otlk", "Day 0") : Button.secondary("day0otlk", "Day 0")),
+					(event.getComponentId().equals("day1otlk") ? Button.primary("day1otlk", "Day 1") : Button.secondary("day1otlk", "Day 1")),
+					(event.getComponentId().equals("day2otlk") ? Button.primary("day2otlk", "Day 2") : Button.secondary("day2otlk", "Day 2")),
+					(event.getComponentId().equals("day3otlk") ? Button.primary("day3otlk", "Day 3") : Button.secondary("day3otlk", "Day 3")),
+					(event.getComponentId().equals("day48otlk") ? Button.primary("day48otlk", "Day 4-8") : Button.secondary("day48otlk", "Day 4-8"))
+				).queue();
+		
 		if (event.getComponentId().equals("day48otlk")) {
 			MessageEmbed newEmbed = new EmbedBuilder()
 					//Simple reference to their resource file
-					.setImage("https://www.spc.noaa.gov/products/outlook/day3otlk_0730.gif?" + Long.toString(Math.round(Math.random() * 100000))) //Append meaningless query to escape previously cached image
+					.setImage("https://www.spc.noaa.gov/products/exper/day4-8/day48prob.gif?" + Long.toString(Math.round(Math.random() * 100000))) //Append meaningless query to escape previously cached image
 					.build();
 			
 			event.editMessageEmbeds(newEmbed).queue();
-			event.editComponents().setActionRow(
-					Button.secondary("day0otlk","Day 0"),
-					Button.secondary("day1otlk", "Day 1"),
-					Button.secondary("day2otlk", "Day 2"),
-					Button.secondary("day3otlk", "Day 3"),
-					Button.primary("day48otlk", "Day 4-8")
-					);
+		} else {
+			System.out.println("Testing!");
+//			event.editComponents().setActionRow(
+//					Button.danger("waar", "whar")
+//					)
+//			.queue();
 		}
 	}
 }
